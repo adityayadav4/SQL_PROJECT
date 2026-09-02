7a58d2a9c04543bac96915a0f49fd7bcb6dc6a71
adityayadav4 <vasu198222@gmail.com>
2026-09-03 01:53:15 +0530

    first commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,16 @@
+COPY company_dim
+FROM 'E:\POSTGRESSQLPROJECT\csv_files\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'E:\POSTGRESSQLPROJECT\csv_files\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'E:\POSTGRESSQLPROJECT\csv_files\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'E:\POSTGRESSQLPROJECT\csv_files\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
